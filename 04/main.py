86ba4631d803f707236fcd0af899aa3dfcde5053
Rick Bischoff <rdbisch@gmail.com>
2023-12-08 08:42:43 -0600

    part 2

--- 04/main.py
+++ 04/main.py
@@ -4,15 +4,26 @@ import numpy as np
 if __name__ == "__main__":
     result = 0
     
+    copies = {}
+    total = 0
+
     for line in sys.stdin:
         line = line.strip()
-        winners = set(line.split("|")[0].split(' '))
-        numbers = set(line.split("|")[1].split(' '))
+        tmp = line.split(':')
+        card_num = int(tmp[0].split(' ')[-1])
+
+        copies[card_num] = copies.get(card_num, 0) + 1
+        total += copies[card_num]
+
+        tmp2 = tmp[1].split('|')
+        winners = set(tmp2[0].split(' '))
+        numbers = set(tmp2[1].split(' '))
         if '' in winners: winners.remove('')
         if '' in numbers: numbers.remove('')
         inter = winners.intersection(numbers)
         print(f"{line} {winners} {numbers} {inter}")
         if len(inter) > 0:
-            result += np.power(2, len(inter) - 1)
+            for new_card in range(card_num  + 1, card_num + 1 + len(inter)):
+                copies[new_card] = copies.get(new_card, 0) + copies[card_num]
 
-    print(result)
+    print(total)
